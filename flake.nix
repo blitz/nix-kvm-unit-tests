45911fb784728706ba5a2bdd63afbd7270e58534
Julian Stecklina <js@alien8.de>
2022-03-03 23:06:31 +0100

    Add Hercules CI compatiblity

--- flake.nix
+++ flake.nix
@@ -7,12 +7,28 @@
       url = "gitlab:kvm-unit-tests/kvm-unit-tests/master";
       flake = false;
     };
+
+    # For Hercules CI
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    flake-compat-ci.url = "github:hercules-ci/flake-compat-ci";
   };
 
-  outputs = { self, nixpkgs, kvm-unit-tests }: {
+  outputs = { self, nixpkgs, kvm-unit-tests, flake-compat, flake-compat-ci }: {
     packages.x86_64-linux.kvm-unit-tests = nixpkgs.legacyPackages.x86_64-linux.callPackage ./kvm-unit-tests.nix {
       src = kvm-unit-tests;
     };
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.kvm-unit-tests;
+
+    # For Hercules CI, which doesn't natively support flakes (yet).
+    ciNix = flake-compat-ci.lib.recurseIntoFlakeWith {
+      flake = self;
+
+      # Optional. Systems for which to perform CI.
+      systems = [ "x86_64-linux" ];
+    };
+
   };
 }
